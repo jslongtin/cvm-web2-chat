5b870e6087e9361b0e81a33a5db4fb30f0a84673
Jessika Longtin <JessikaLongtin@hotmail.com>
2022-08-16 15:15:38 -0400

    ajout dog

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,4 +1,5 @@
-import {signin} from './chat-api';
+import { signin } from './chat-api';
+import Dog from './sprite/Dog.js';
 
 let spriteList = [];
 
@@ -7,11 +8,16 @@ window.addEventListener("load", () => {
         return signin(this);
     }
 
+    let node = document.createElement("div");
+    node.id = "dog";
+    let parentNode = document.querySelector(".index");
+    parentNode.append(node);
+    spriteList.push(new Dog("dog"));
     tick();
 });
 
 const tick = () => {
-  
+
     for (let i = 0; i < spriteList.length; i++) {
         const sprite = spriteList[i];
         let alive = sprite.tick();
--- client/src/sprite/Dog.js
+++ client/src/sprite/Dog.js
@@ -1,26 +1,14 @@
-class Dog{
-
+export default class Dog{
     constructor(id){
-        // this pour conserver la vriable, let va la supprimer en sortant du constructeur
         this.node = document.querySelector("#" + id);
-        this.node.style.top = (Math.random() * 40)+ "px";
-        this.node.style.left = (Math.random() * 400) + "px";
+        this.node.style.backgroundImage = "url('X:/gitlocal/cvm-web2-chat/client/img/sprite sheets/undertale-dog-flowey-toriel-png-favpng-qSC129UT0JTdGzVxJfqmX0yPJ.jpg')"; 
+    
 
-        this.speed = 2;
-        this.velocityY = 0.5; //acceleration
+       
     }
 
-    // va etre appelet chaque 60ms
+    
     tick() {
-        this.speed += this.velocityY;
-        let y = this.node.offsetTop;
-         y += this.speed;
-
-            if( y > 220){
-                this.speed = -this.speed/1.5;
-                y=220;
-            }
-
-         this.node.style.top = y + "px";
+     
     }
 }

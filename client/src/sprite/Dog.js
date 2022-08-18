284cd41027a7d6a87d6611f9168e098cdc190e4d
jslontgin <jessikalongtin@hotmail.com>
2022-08-18 19:23:32 -0400

    erttre

--- client/src/sprite/Dog.js
+++ client/src/sprite/Dog.js
@@ -1,8 +1,13 @@
 export default class Dog{
     constructor(id){
         this.node = document.querySelector("#" + id);
-        // this.node.style.backgroundImage = "url('X:/gitlocal/cvm-web2-chat/client/img/sprite sheets/undertale-dog-flowey-toriel-png-favpng-qSC129UT0JTdGzVxJfqmX0yPJ.jpg')"; 
-    
+        this.node.style.backgroundImage = "url('../../img/sprite sheets/6678d658b4c099e26c077bbc81d0a2e2.gif')"; 
+        this.node.style.backgroundPosition = "center";
+        this.node.style.width = 50 + "px";
+        this.node.style.height = 50 + "px";
+        this.node.style.position = "absolute"
+        this.node.style.right = 0 + "px";
+        this.node.style.bottom = 0 + "px";S
 
        
     }
